8a8687eca99d71cfbf9816a746db12b58755ed4e
Ahn GyuTae <xpsj20@gmail.com>
2025-07-13 23:41:40 +0900

    fix: worse layout, bugs
    - 몇 레이아웃 중 잘못 설정된 부분 수정
    - 이미지나 정상적으로 치환되지 않던 문제 해결

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,21 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite:{ 
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (e) => {
+            if(e.names?.[0].endsWith('.css')) {
+              return 'style.css';
+            }
+            return 'assets/[name]-[hash][extname]';
+          }
+        }
+      }
+    }
+  },
+  build: {
+    format: 'file',
   }
 });
--- src/scripts/common.ts
+++ src/scripts/common.ts
@@ -1,9 +1,9 @@
-import seqSnippet from "./snippets.ts";
-import { LoadedTheme } from "./toggleTheme.ts";
-import setOffcanvasEvents from "./offcanvas.ts";
-import changeSubsBtnIco from "./changeSubsBtn.ts";
-//import handleScroll from "./navbarDoc.ts";
-//import setEventListenersOnDropdown from "./dropdown.ts";
+import seqSnippet from "./snippets";
+import { LoadedTheme } from "./toggleTheme";
+import setOffcanvasEvents from "./offcanvas";
+import changeSubsBtnIco from "./changeSubsBtn";
+//import handleScroll from "./navbarDoc";
+//import setEventListenersOnDropdown from "./dropdown";
 
 // 페이지가 로드되면 스니펫 애니메이션 시작
 seqSnippet();
