8ae6d1a393771fb02c122ab626b6322fbe4d4b0b
Ahn GyuTae <xpsj20@gmail.com>
2025-07-20 20:17:20 +0900

    chore: add @tistory-skin-previewer
    https://www.npmjs.com/package/@tistory-skin-previewer/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import tistoryPreviewer from '@tistory-skin-previewer/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [ tistoryPreviewer() ],
   vite: {
     plugins: [
       // @ts-ignore
